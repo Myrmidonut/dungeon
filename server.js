1b69d3bda9eb862ab58cca46c1f6a5f9f078a0c1
Frederik Funke <frederik-funke@gmx.de>
2019-04-20 00:15:55 +0200

    added basic react frontend with express route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const jwt = require("express-jwt")
 const graphqlHTTP = require('express-graphql');
+const path = require("path")
 require("dotenv").config()
 
 const schema = require("./graphql/schema")
@@ -14,16 +15,17 @@ const auth = jwt({
   credentialsRequired: false
 })
 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 app.use(auth)
 
-app.use((err, req, res, next) => {
+/*app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     console.log("Bad token.")
   }
 
   next()
-});
+});*/
 
 app.use('/graphql', graphqlHTTP(req => ({
   schema: schema,
@@ -33,6 +35,10 @@ app.use('/graphql', graphqlHTTP(req => ({
   },
   graphiql: true
 })))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Express server running on http://localhost:${port}/graphql`)
